5c894d24f44ac8eea8839b95e3c1026520cc5267
MOO-SHU <mooshucode@gmail.com>
2022-10-04 23:36:48 +0200

    added the JavaScript part, added onClick +1/+2/+3

---
+++ index.js
@@ -0,0 +1,44 @@
+//  -- Home Score --
+
+
+let homeScoreEl = document.getElementById('home-score');
+let homeScore = 0;
+
+function increaseHomeScore() {
+    homeScore += 1;
+    homeScoreEl.textContent = homeScore;
+
+}
+
+function increaseHomeScore2() {
+    homeScore += 2;
+    homeScoreEl.textContent = homeScore;
+
+}
+
+function increaseHomeScore3() {
+    homeScore += 3;
+    homeScoreEl.textContent = homeScore;
+
+}
+
+// -- Guest Score --
+
+let guestScoreEl = document.getElementById('guest-score');
+let guestScore = 0;
+
+function increaseGuestScore1(){
+    guestScore += 1;
+    guestScoreEl.textContent = guestScore;
+}
+
+function increaseGuestScore2(){
+    guestScore += 2;
+    guestScoreEl.textContent = guestScore;
+}
+
+function increaseGuestScore3(){
+    guestScore += 3;
+    guestScoreEl.textContent = guestScore;
+}
+
